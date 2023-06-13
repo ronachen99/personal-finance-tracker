2b60a9a9cbbf997f7e4211ec41fd03216fbbc037
Rona C <rona.chen@hotmail.com>
2023-06-13 14:24:23 -0400

    changed the wording

--- seeds/incomeData.js
+++ seeds/incomeData.js
@@ -2,17 +2,17 @@ const { Income } = require('../models');
 
 const incomeData = [
   {
-    income_name: 'salary',
+    income_name: 'Salary',
     income_amount: 100000,
     user_id: 1
   },
   {
-    income_name: 'side hustle',
+    income_name: 'Side Hustle',
     income_amount: 30000,
     user_id: 2
   },
   {
-    income_name: 'incentive',
+    income_name: 'Incentive',
     income_amount: 2000,
     user_id: 3
   }
