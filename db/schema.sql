80f4fd3bd998d0eb8ebc76d3f54b91fbffe8f065
Tyler Vick <jrtvick@gmail.com>
2023-06-13 14:45:51 -0400

    updated user model and seeded user data

---
+++ db/schema.sql
@@ -0,0 +1,5 @@
+-- Delete the database if it exists, else create it
+DROP DATABASE IF EXISTS finance_tracker_db;
+CREATE DATABASE finance_tracker_db;
+-- Use the database
+USE finance_tracker_db;
---
+++ seeds/userData.js
@@ -0,0 +1,17 @@
+[
+    {
+      "name": "Sal",
+      "email": "sal@hotmail.com",
+      "password": "password12345"
+    },
+    {
+      "name": "Mark",
+      "email": "mark@hotmail.com",
+      "password": "password12345"
+    },
+    {
+      "name": "Kyla",
+      "email": "kyla@hotmail.com",
+      "password": "password12345"
+    }
+  ]
