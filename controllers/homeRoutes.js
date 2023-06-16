d739199ce494e161564b0421890e0a2bcdea26e4
Rona C <rona.chen@hotmail.com>
2023-06-16 17:14:50 -0400

    fixed view report bug and addeed css to the homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -79,49 +79,50 @@ router.get('/income', withAuth, async (req, res) => {
 });
 
 router.get('/report', withAuth, async (req, res) => {
-  try {
-    res.render('report', { logged_in: req.session.logged_in });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+  const { month, year } = req.query;
+  if (!month || !year) {
+    try {
+      res.render('report', { logged_in: req.session.logged_in });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    try {
+      const incomeData = await Income.findAll({
+        where: {
+          user_id: req.session.user_id,
+          month: req.query.month,
+          year: req.query.year
+        }
+      });
+      const incomes = incomeData.map((income) => income.get({ plain: true }));
 
-// Use withAuth middleware to prevent access to route
-router.get('/report/:year/:month', withAuth, async (req, res) => {
-  console.log(req);
-  try {
-    const incomeData = await Income.findAll({
-      where: {
-        user_id: req.session.user_id,
-        month: req.params.month,
-        year: req.params.year
-      }
-    });
-    const incomes = incomeData.map((income) => income.get({ plain: true }));
+      const expenseData = await Expense.findAll({
+        where: {
+          user_id: req.session.user_id,
+          month: req.query.month,
+          year: req.query.year
+        }
+      });
 
-    const expenseData = await Expense.findAll({
-      where: {
-        user_id: req.session.user_id,
-        month: req.params.month,
-        year: req.params.year
-      }
-    });
+      const expenses = expenseData.map((expense) =>
+        expense.get({ plain: true })
+      );
 
-    const expenses = expenseData.map((expense) => expense.get({ plain: true }));
+      console.log({
+        incomes,
+        expenses,
+        logged_in: req.session.logged_in
+      });
 
-    console.log({
-      incomes,
-      expenses,
-      logged_in: req.session.logged_in
-    });
-
-    res.render('report', {
-      incomes,
-      expenses,
-      logged_in: req.session.logged_in
-    });
-  } catch (err) {
-    res.status(500).json(err);
+      res.render('report', {
+        incomes,
+        expenses,
+        logged_in: req.session.logged_in
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
   }
 });
 
