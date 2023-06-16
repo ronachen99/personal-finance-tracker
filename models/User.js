6b6b08cc7c445b14b839f74a223dcf7720aa4976
Tyler Vick <jrtvick@gmail.com>
2023-06-15 21:44:15 -0400

    edited the homeRoutes and User model to be able to render the user's name on the homepage handlebar, as well as updated the helpers util and created a 404 landing page for any errors

--- models/User.js
+++ models/User.js
@@ -18,6 +18,10 @@ User.init(
       primaryKey: true,
       autoIncrement: true
     },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
     email: {
       type: DataTypes.STRING,
       allowNull: false,
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,12 +1,6 @@
-const dayjs = require('dayjs');
-
 module.exports = {
     format_date: (date) => {
       // Format date as MM/DD/YYYY
       return date.toLocaleDateString();
-    },
-
-    format_month: (date) => {
-        return dayjs(date).format('MMMM');
     }
   };
