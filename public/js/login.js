b6606cfe541ad13b9eec304049cdf5a3a3e60909
Rona C <rona.chen@hotmail.com>
2023-06-16 13:36:22 -0400

    added button

--- public/js/login.js
+++ public/js/login.js
@@ -1,54 +1,59 @@
-document.querySelector('.login-form').addEventListener('submit', async (event) => {
-  event.preventDefault();
+document
+  .querySelector('.login-form')
+  .addEventListener('submit', async (event) => {
+    event.preventDefault();
 
-  const email = document.querySelector('#email-login').value.trim();
-  const password = document.querySelector('#password-login').value.trim();
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
 
-  if (email && password) {
-    const response = await fetch('/api/users/login', {
-      method: 'POST',
-      body: JSON.stringify({ email, password }),
-      headers: { 'Content-Type': 'application/json' }
-    });
+    if (email && password) {
+      const response = await fetch('/api/users/login', {
+        method: 'POST',
+        body: JSON.stringify({ email, password }),
+        headers: { 'Content-Type': 'application/json' }
+      });
 
-    if (response.ok) {
-      const successNotification = document.createElement('div');
-      successNotification.className = 'notification is-success';
-      successNotification.innerHTML = `
-      🤠 Welcome back partner! 🤠
+      if (response.ok) {
+        const successNotification = document.createElement('div');
+        successNotification.className = 'notification is-success';
+        successNotification.innerHTML = `
+      🤠 welcome back partner 🤠
       `;
 
-      const notificationContainer = document.querySelector('.notification-container');
-      notificationContainer.appendChild(successNotification);
+        const notificationContainer = document.querySelector(
+          '.notification-container'
+        );
+        notificationContainer.appendChild(successNotification);
 
-      setTimeout(function() {
-        successNotification.classList.add('fade-out');
-        setTimeout(function() {
-          document.location.replace('/');
+        setTimeout(function () {
+          successNotification.classList.add('fade-out');
+          setTimeout(function () {
+            document.location.replace('/');
+          }, 800);
         }, 800);
-      }, 800);
 
-      successNotification.addEventListener('animationend', function() {
-        successNotification.parentNode.removeChild(successNotification);
-      });
-    } else {
-      const errorNotification = document.createElement('div');
-      errorNotification.className = 'notification is-danger';
-      errorNotification.innerHTML = `
-        ┐(⎚ ꞈ ⎚)┌ Something went wrong
+        successNotification.addEventListener('animationend', function () {
+          successNotification.parentNode.removeChild(successNotification);
+        });
+      } else {
+        const errorNotification = document.createElement('div');
+        errorNotification.className = 'notification is-danger';
+        errorNotification.innerHTML = `
+        ┐(⎚ ꞈ ⎚)┌  wrong email or password
       `;
 
-      const notificationContainer = document.querySelector('.notification-container');
-      notificationContainer.appendChild(errorNotification);
+        const notificationContainer = document.querySelector(
+          '.notification-container'
+        );
+        notificationContainer.appendChild(errorNotification);
 
-      setTimeout(function() {
-        errorNotification.classList.add('fade-out');
-      }, 1750);
+        setTimeout(function () {
+          errorNotification.classList.add('fade-out');
+        }, 1750);
 
-      errorNotification.addEventListener('animationend', function() {
-        errorNotification.parentNode.removeChild(errorNotification);
-      });
+        errorNotification.addEventListener('animationend', function () {
+          errorNotification.parentNode.removeChild(errorNotification);
+        });
+      }
     }
-  }
-});
-
+  });
