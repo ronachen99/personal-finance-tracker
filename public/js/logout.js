cf918db2b3fa961e0857fc76aa2c589c2eb0a7ef
Rona C <rona.chen@hotmail.com>
2023-06-16 22:51:12 -0400

    made all buttons round and also made changes to the text of the button

--- public/js/logout.js
+++ public/js/logout.js
@@ -5,7 +5,7 @@ const logout = async () => {
   logoutModal.classList.add('is-active');
 
   const successNotification = document.createElement('div');
-  successNotification.className = 'notification is-success';
+  successNotification.className = 'notification is-black';
   successNotification.innerHTML = `🤠 safe travels, partner 🤠`;
 
   // Get the notification container within the modal
